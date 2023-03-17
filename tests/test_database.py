1f52f76b93653874d88b92ee303fd8f60a5302f0
francoEmunoz <munozfranco011@gmail.com>
2023-03-17 00:58:02 -0300

    tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,6 +1,9 @@
 import unittest
 import database as db
 import copy
+import helpers
+import config
+import csv
 
 class TestDatabase(unittest.TestCase):
 
@@ -13,14 +16,14 @@ class TestDatabase(unittest.TestCase):
 
     def test_buscar_cliente(self):
         cliente_existente = db.Clientes.buscar('43900419')
-        cliente_inexistente = db.Clientes.buscar('20900419')
+        cliente_inexistente = db.Clientes.buscar('43900555')
         self.assertIsNotNone(cliente_existente)
         self.assertIsNone(cliente_inexistente)
 
     def test_crear_cliente(self):
-        nuevo_cliente = db.Clientes.crear('43840326', 'Nahuel', 'Oblitas')
+        nuevo_cliente = db.Clientes.crear('20099840', 'Nahuel', 'Oblitas')
         self.assertEqual(len(db.Clientes.lista), 4)
-        self.assertEqual(nuevo_cliente.dni, '43840326')
+        self.assertEqual(nuevo_cliente.dni, '20099840')
         self.assertEqual(nuevo_cliente.nombre, 'Nahuel')
         self.assertEqual(nuevo_cliente.apellido, 'Oblitas')
 
@@ -35,3 +38,22 @@ class TestDatabase(unittest.TestCase):
         cliente_rebuscado = db.Clientes.buscar('32904468')
         self.assertEqual(cliente_borrado.dni, '32904468')
         self.assertIsNone(cliente_rebuscado)
+
+    def test_validar_dni(self):
+        self.assertTrue(helpers.dni_valido('42881111',db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('holahola',db.Clientes.lista))
+        self.assertFalse(helpers.dni_valido('43900419',db.Clientes.lista))
+
+    def test_escritura_csv(self):
+        db.Clientes.borrar('42030999')
+        db.Clientes.borrar('32904468')
+        db.Clientes.editar('43900419', 'Hugo', 'Montoya')
+    
+        dni, nombre, apellido = None, None, None
+        with open(config.DATABASE_PATH, newline="\n") as csvfile:
+            reader = csv.reader(csvfile, delimiter=";")
+            dni, nombre, apellido = next(reader)
+    
+        self.assertEqual(dni, '43900419')
+        self.assertEqual(nombre, 'Hugo')
+        self.assertEqual(apellido, 'Montoya')
